87b0dd4397f666f45fab839ac0e47017d1e66c64
JeremiD0 <damski.jeremi00@gmail.com>
2023-02-21 13:55:36 +0100

    MainActivity skonczone

--- app/src/main/java/com/example/seekbar2102/MainActivity.kt
+++ app/src/main/java/com/example/seekbar2102/MainActivity.kt
@@ -1,11 +1,132 @@
 package com.example.seekbar2102
 
-import androidx.appcompat.app.AppCompatActivity
+import android.annotation.SuppressLint
+import android.graphics.Bitmap
+import android.graphics.drawable.BitmapDrawable
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.ProgressBar
+import android.widget.SeekBar
+import android.widget.SeekBar.OnSeekBarChangeListener
+import androidx.appcompat.app.AppCompatActivity
+
 
 class MainActivity : AppCompatActivity() {
+
+    fun suma_poziomu(): Int{
+        val obraz1_poziom = findViewById<SeekBar>(R.id.poziom1)
+        val obraz2_poziom = findViewById<SeekBar>(R.id.poziom2)
+        val obraz3_poziom = findViewById<SeekBar>(R.id.poziom3)
+        val suma = obraz1_poziom.progress + obraz2_poziom.progress + obraz3_poziom.progress
+        return suma
+    }
+
+    fun suma_pionu(): Int{
+        val obraz1_pion = findViewById<SeekBar>(R.id.pion1)
+        val obraz2_pion = findViewById<SeekBar>(R.id.pion2)
+        val obraz3_pion = findViewById<SeekBar>(R.id.pion3)
+        val suma = obraz1_pion.progress + obraz2_pion.progress + obraz3_pion.progress
+        return suma
+    }
+
+    fun changed(progressBar: ProgressBar, suma: Int){
+        progressBar.progress = suma/3
+    }
+
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val obraz1 = findViewById<ImageView>(R.id.imv1)
+        val obraz2 = findViewById<ImageView>(R.id.imv2)
+        val obraz3 = findViewById<ImageView>(R.id.imv3)
+
+        val obraz1_poziom = findViewById<SeekBar>(R.id.poziom1)
+        val obraz1_pion = findViewById<SeekBar>(R.id.pion1)
+        val obraz2_poziom = findViewById<SeekBar>(R.id.poziom2)
+        val obraz2_pion = findViewById<SeekBar>(R.id.pion2)
+        val obraz3_poziom = findViewById<SeekBar>(R.id.poziom3)
+        val obraz3_pion = findViewById<SeekBar>(R.id.pion3)
+        val reset = findViewById<Button>(R.id.reset)
+        val progress_poziom = findViewById<ProgressBar>(R.id.pgbpoziom)
+        val progress_pion = findViewById<ProgressBar>(R.id.pgbpion)
+
+        reset.setOnClickListener() {
+            obraz1_pion.progress = 100
+            obraz2_pion.progress = 100
+            obraz3_pion.progress = 100
+            obraz1_poziom.progress = 100
+            obraz2_poziom.progress = 100
+            obraz3_poziom.progress = 100
+        }
+
+        obraz1_pion.setOnSeekBarChangeListener(object:OnSeekBarChangeListener{
+            override fun onProgressChanged(seekBar: SeekBar,progres: Int, fromUser: Boolean) {
+                val scale = progres/100f
+                obraz1.scaleY = scale
+                changed(progress_pion, suma_pionu())
+            }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar) {}
+            override fun onStopTrackingTouch(seekBar: SeekBar) {}
+        })
+
+        obraz1_poziom.setOnSeekBarChangeListener(object:OnSeekBarChangeListener{
+            override fun onProgressChanged(seekBar: SeekBar,progres: Int, fromUser: Boolean) {
+                val scale = progres/100f
+                obraz1.scaleX = scale
+                changed(progress_poziom, suma_poziomu())
+            }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar) {}
+            override fun onStopTrackingTouch(seekBar: SeekBar) {}
+        })
+
+        obraz2_poziom.setOnSeekBarChangeListener(object:OnSeekBarChangeListener{
+            override fun onProgressChanged(seekBar: SeekBar,progres: Int, fromUser: Boolean) {
+                val scale = progres/100f
+                obraz2.scaleX = scale
+                changed(progress_poziom, suma_poziomu())
+            }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar) {}
+            override fun onStopTrackingTouch(seekBar: SeekBar) {}
+        })
+
+        obraz2_pion.setOnSeekBarChangeListener(object:OnSeekBarChangeListener{
+            override fun onProgressChanged(seekBar: SeekBar,progres: Int, fromUser: Boolean) {
+                val scale = progres/100f
+                obraz2.scaleY = scale
+                changed(progress_pion, suma_pionu())
+            }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar) {}
+            override fun onStopTrackingTouch(seekBar: SeekBar) {}
+        })
+
+        obraz3_pion.setOnSeekBarChangeListener(object:OnSeekBarChangeListener{
+            override fun onProgressChanged(seekBar: SeekBar,progres: Int, fromUser: Boolean) {
+                val scale = progres/100f
+                obraz3.scaleY = scale
+                changed(progress_pion, suma_pionu())
+            }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar) {}
+            override fun onStopTrackingTouch(seekBar: SeekBar) {}
+        })
+
+        obraz3_poziom.setOnSeekBarChangeListener(object:OnSeekBarChangeListener{
+            override fun onProgressChanged(seekBar: SeekBar,progres: Int, fromUser: Boolean) {
+                val scale = progres/100f
+                obraz3.scaleX = scale
+                changed(progress_poziom, suma_poziomu())
+            }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar) {}
+            override fun onStopTrackingTouch(seekBar: SeekBar) {}
+        })
     }
+
 }
